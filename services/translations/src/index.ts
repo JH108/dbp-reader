1cb03f7a3fb866ee8cadb89d3fae163813a59bb1
Jesse Hill <jesse_hill@eogresources.com>
2019-11-19 08:22:08 -0600

    Work to use the json files instead of just js files

--- services/translations/src/index.ts
+++ services/translations/src/index.ts
@@ -12,10 +12,6 @@ const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const globFile = promisify(glob);
 
-const MESSAGE_FILES_PATTERN: string = "../../app/**/messages.{mjs,js}";
-const LANG_DIR: string = "../../languages-test/locales/";
-const LANG_PATTERN: string = "../../languages-test/locales/*.json";
-
 /** Structure
  * Steps
  * 1. Gather all messages from application
@@ -39,31 +35,56 @@ const LANG_PATTERN: string = "../../languages-test/locales/*.json";
 // TODO: Extract all default messages into json files and then update the js files to import them
 // Going the path of using regex is going to be too unstable and will require more effort
 class TranslationGenerator {
-  // If the glob function needs to grow put it here
-  async globFiles() {}
-
-  public messageObjectRegex = new RegExp(
+  constructor(baseJSON?: {}) {
+    if (baseJSON) {
+      this.messages = baseJSON;
+    }
+  }
+  private messages: {};
+  private MESSAGE_FILES_PATTERN: string = "../../app/**/messages.json";
+  private LANG_DIR: string = "../../languages-test/locales/";
+  private LANG_PATTERN: string = "../../languages-test/locales/*.json";
+  private messageObjectRegex = new RegExp(
     /export default defineMessages\(\{(.*)\}/gms
   );
-  // Has issues around the [\s|^\}] part, the regex is being too greedy...
-
-  public messagePairRegex = new RegExp(
+  private messagePairRegex = new RegExp(
     /(?<id>id:\s*['"]app\.(components|containers).*?['"])(,\s*)(?<defaultMessage>defaultMessage:\s*['"].*?['"])/g
   );
 
+  private handleReturnedJSON(data: {}) {
+    // update the local messages object with the new messages retrieved from the JSON files
+    return this.messages;
+  }
+
   public removeNewlineAndTab = (data: string) => data.replace(/\n\t/g, "");
 
-  async readFiles(searchPattern: string) {
+  async globFiles(searchPattern: string) {
+    const fileNames = await globFile(searchPattern);
+
+    return fileNames;
+  }
+
+  async readFiles(searchPattern: string = this.MESSAGE_FILES_PATTERN) {
     // Get filenames
-    const filenames = await globFile(searchPattern);
+    const filenames = await this.globFiles(searchPattern);
     // Map files to promises
     const filenamePromises = filenames.map(async (file: string) => {
       const filePath = path.join(file);
-      const fileBuffer = await readFile(filePath);
-      const fileData = Buffer.from(fileBuffer).toString("utf8");
+      let fileData = "";
+      console.log("filePath.match(/$.json/)", filePath.match(/$\.json/));
+      console.log("filePath.match(/.json$/)", filePath.match(/\.json$/));
+
+      if (filePath.match(/$\.json/)) {
+        // JSON can be imported
+        const json = require(filePath);
+        fileData = JSON.parse(json);
+      } else {
+        // Everything else needs to be read and parsed
+        const fileBuffer = await readFile(filePath);
+        fileData = Buffer.from(fileBuffer).toString("utf8");
+      }
       console.log("\nFILE-PATH:", filePath, "\n");
-      const data = await import(filePath);
-      console.log("\nDATA:", data, "\n");
+      console.log("\nDATA:", fileData, "\n");
 
       return [file, fileData];
     });
@@ -72,14 +93,19 @@ class TranslationGenerator {
 
     const parsedFilePromises = filenameData.map(
       async ([fileName, data]: string): Promise<string> => {
-        const matchedData = data.match(this.messageObjectRegex);
-        const normMatchedData =
-          matchedData && matchedData[0] ? matchedData[0] : "";
-        const parsedText = this.removeNewlineAndTab(normMatchedData);
-        const slicedText = parsedText.slice(30);
-        const messageJson = JSON.stringify(slicedText);
-        await this.writeFile(fileName, messageJson, true);
-
+        if (typeof data !== "string") {
+          this.handleReturnedJSON(data);
+        } else {
+          // Need a helper method to resolve the files that are strings and one to resolve the JSON objects
+          const matchedData = data.match(this.messageObjectRegex);
+          const normMatchedData =
+            matchedData && matchedData[0] ? matchedData[0] : "";
+          const parsedText = this.removeNewlineAndTab(normMatchedData);
+          const slicedText = parsedText.slice(30);
+          const messageJson = JSON.stringify(slicedText);
+          // Do something else here like check to see if the json file already exists
+          await this.writeFile(fileName, messageJson, true);
+        }
         return fileName;
       }
     );
@@ -92,20 +118,20 @@ class TranslationGenerator {
 
   async writeFile(fileName: string, data: string, dryrun?: boolean) {
     const normFileName = fileName.replace(/\.js$/, ".json");
+
     if (dryrun) {
-      // console.log("\nWrote:", normFileName, "\nData:\n", data, "\n");
       return `Wrote: ${normFileName}...`;
     }
+
     const result = await writeFile(normFileName, data);
+
     return result;
   }
 }
 
-const init = async () => {
+const init = async (messagePattern?: string) => {
   const translationGenerator = new TranslationGenerator();
-  const messageFiles = await translationGenerator.readFiles(
-    MESSAGE_FILES_PATTERN
-  );
+  const messageFiles = await translationGenerator.readFiles(messagePattern);
 
   console.log("messageFiles", messageFiles && messageFiles.length);
 
